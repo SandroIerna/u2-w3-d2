b3e59e79b5e9b5d2363d84879ee1e1ab4de3d6b7
SandroIerna <iernasandro@gmail.com>
2023-01-17 16:00:53 +0100

    adds finished BE

---
+++ src/api/blogs/model.js
@@ -0,0 +1,23 @@
+import mongoose, { model } from "mongoose";
+
+const { Schema } = mongoose;
+
+const blogsSchema = new Schema(
+  {
+    category: { type: String, required: true },
+    title: { type: String, required: true },
+    cover: { type: String, required: true },
+    readTime: {
+      value: { type: Number, required: true },
+      unit: { type: String, required: true },
+    },
+    author: {
+      name: { type: String, required: true },
+      avatar: { type: String, required: false },
+    },
+    content: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export default model("Blog", blogsSchema);
---
+++ src/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import cors from "cors";
+import mongoose from "mongoose";
+import blogsRouter from "./api/blogs/index.js";
+import {
+  badRequestHandler,
+  genericErrorHandler,
+  notFoundHandler,
+} from "./errorHandlers.js";
+
+const server = express();
+const port = 3001;
+
+// ****************************** MIDDLEWARES ******************************
+
+server.use(cors());
+server.use(express.json());
+
+// ******************************* ENDPOINTS *******************************
+
+server.use("/blogs", blogsRouter);
+
+// ***************************** ERROR HANDLERS ****************************
+
+server.use(badRequestHandler);
+server.use(notFoundHandler);
+server.use(genericErrorHandler);
+
+mongoose.connect(process.env.MONGO_URL);
+
+mongoose.connection.on("connected", () => {
+  server.listen(port, () => {
+    console.table(listEndpoints(server));
+    console.log(`Server is running on port ${port}`);
+  });
+
+  console.log("Successfully connected to Mongo!");
+});

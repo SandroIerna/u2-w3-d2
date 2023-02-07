96290f29871447a75a5c38f4e77d362a7316067f
SandroIerna <iernasandro@gmail.com>
2023-02-07 22:46:35 +0100

    adds auth

--- src/api/authors/model.js
+++ src/api/authors/model.js
@@ -1,14 +1,52 @@
 import mongoose, { model } from "mongoose";
+import bcrypt from "bcrypt";
 
-const { Schema } = mongoose;
+const { Schema, model } = mongoose;
 
 const authorSchema = new Schema(
   {
     name: { type: String, required: true },
     avatar: { type: String, required: false },
     blogPosts: [{ type: Schema.Types.ObjectId, ref: "Blog" }],
+    password: { type: String, required: true },
+    role: { type: String, enum: ["User", "Admin"], default: "User" },
   },
   { timestamps: true }
 );
+
+authorSchema.pre("save", async function (next) {
+  const currentAuthor = this;
+  if (currentAuthor.isModified("password")) {
+    const plainPW = currentAuthor.password;
+    const hash = await bcrypt.hash(plainPW, 11);
+    currentAuthor.password = hash;
+  }
+  next();
+});
+
+authorSchema.methods.toJSON = function () {
+  const authorDocument = this;
+  const author = authorDocument.toObject();
+
+  delete author.password;
+  delete author.createdAt;
+  delete author.updatedAt;
+  delete author.__v;
+  return author;
+};
+
+authorSchema.static("checkCredentials", async function (email, password) {
+  const author = await this.findOne({ email });
+  if (author) {
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (passwordMatch) {
+      return author;
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+});
 
 export default model("Author", authorSchema);
